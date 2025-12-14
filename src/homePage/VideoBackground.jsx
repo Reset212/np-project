8481a59f9c548bf86d2e97b643afa0243a87a99e
0_X1mik_0 <reset212banko@gmail.com>
2025-12-14 17:50:36 +0500

    без кнопки

--- src/homePage/VideoBackground.jsx
+++ src/homePage/VideoBackground.jsx
@@ -301,15 +301,7 @@ const VideoBackground = () => {
           <div className={`mobile-dropdown ${isMenuOpen ? 'active' : ''}`}>
             {isMenuOpen && (
               <>
-                <button 
-                  className="close-menu"
-                  onClick={closeMenu}
-                  aria-label="Close menu"
-                >
-                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1 1L19 19M19 1L1 19" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-                  </svg>
-                </button>
+
                 
                 <Link 
                   to="/real-estate" 
