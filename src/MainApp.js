e189210e91f25aeee485c75517bdc0a401433a0a
dse-dse <Manatin228306@Mail.ru>
2026-01-27 20:27:39 +0500

    Update MainApp.js

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import HomePageApp from "./homePage/App";
-import RealEstatePageApp from "./realEstatePage/App";
-import ProjectsPageApp from "./projectsPage/App"; 
-import PrivacyPolicyApp from "./privacyPolicy/App"; 
-import TermsOfUseApp from "./termsOfUse/App"; 
-import AccessibilityStatementApp from "./accessibilityStatement/App"; 
 import "./MainApp.css";
 import ScrollToTop from "./ScrollToTop";
+
+// Ленивая загрузка всех страниц
+const HomePageApp = lazy(() => import("./homePage/App"));
+const RealEstatePageApp = lazy(() => import("./realEstatePage/App"));
+const ProjectsPageApp = lazy(() => import("./projectsPage/App"));
+const PrivacyPolicyApp = lazy(() => import("./privacyPolicy/App"));
+const TermsOfUseApp = lazy(() => import("./termsOfUse/App"));
+const AccessibilityStatementApp = lazy(() => import("./accessibilityStatement/App"));
+
+// Компонент загрузки
+const PageLoader = () => (
+  <div className="page-loader">
+    <div className="spinner"></div>
+  </div>
+);
 
 function MainApp() {
   return (
     <Router>
       <ScrollToTop />
       <div className="MainApp">
-        <Routes>
-          <Route path="/" element={<HomePageApp />} />
-          <Route path="/home" element={<HomePageApp />} />
-          <Route path="/real-estate" element={<RealEstatePageApp />} />
-          <Route path="/projects" element={<ProjectsPageApp />} /> 
-          <Route path="/privacy-policy" element={<PrivacyPolicyApp />} /> 
-           <Route path="/terms-of-use" element={<TermsOfUseApp />} />
-            <Route path="/accessibility-statement" element={<AccessibilityStatementApp />} /> 
-        </Routes>
+        <Suspense fallback={<PageLoader />}>
+          <Routes>
+            <Route path="/" element={<HomePageApp />} />
+            <Route path="/home" element={<HomePageApp />} />
+            <Route path="/real-estate" element={<RealEstatePageApp />} />
+            <Route path="/projects" element={<ProjectsPageApp />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicyApp />} />
+            <Route path="/terms-of-use" element={<TermsOfUseApp />} />
+            <Route path="/accessibility-statement" element={<AccessibilityStatementApp />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
